3ccd7de63ae2103108f4223a3419d05f820f8475
silentadv <developersilent16@gmail.com>
2025-03-29 12:37:45 -0300

    feat(monorepo): creating monorepo.

---
+++ tests/src/index.ts
@@ -0,0 +1,28 @@
+import { Client } from "../../packages/core/src";
+import { GatewayIntentBits } from "discord-api-types/v10";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+  token: process.env.TOKEN!,
+  totalShardCount: 2,
+});
+
+client.gateway.on("shardConnect", (shard) =>
+  console.log(`shard: ${shard.id} is connected.`)
+);
+
+client.gateway.on("connect", (_) => console.log(`client is connected.`));
+client.gateway.on("messageCreate", (message) => {
+  if (message.content.startsWith("!ping"))
+    return message.channel.write(
+      `Pong! Latency: ${client.gateway.latency()} ms.`
+    );
+});
+
+client.connect();
